d22ef020fede7c5957ce59ff5316fc3a0cc1c5e0
rfwn࿐ <m.erfan.majidi@Gmail.com>
2022-08-13 19:52:46 +0430

    Database

---
+++ src/database/init.ts
@@ -0,0 +1,33 @@
+import { LevelModel } from './levels/levels.model';
+import Client from '../structures/Client';
+import db from 'mongoose';
+
+export default {
+	init: async (client: Client) => {
+		const dbOptions = {
+			useNewUrlParser: true,
+			autoIndex: false,
+			maxPoolSize: 5,
+			connectTimeoutMS: 10000,
+			family: 4,
+			useUnifiedTopology: true,
+		};
+		db.connect(client.config.mongoUrl, dbOptions);
+		db.Promise = global.Promise;
+		db.connection.on('connected', () => {
+			client.logger.info('[+] Database connection initialized.');
+		});
+		db.connection.on('err', (err) => {
+			client.logger.error(`[!] Database has encountered an error: \n ${err.stack}.`);
+		});
+		db.connection.on('disconnected', () => {
+			client.logger.error('[-] Database disconnected.');
+		});
+	},
+	async ping() {
+		const currentNano = process.hrtime();
+		await db.connection.db.command({ ping: 1 });
+		const time = process.hrtime(currentNano);
+		return (time[0] * 1e9 + time[1]) * 1e-6;
+	},
+};
